caea9f85c3e352525eed0c374375612423d33c9f
Ahmet Hakan Karagülle <ahmet.hkn.25@hotmail.com>
2021-01-18 21:26:57 +0300

    Add project files.

---
+++ ClassMethodsDemos/CustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodsDemos
+{
+    class CustomerManager
+    {
+        /*Customer ekleme  metodu (simülasyon)*/
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer added : " + customer.Name);
+        }
+
+        /*Customer silme metodu (simülasyon)*/
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer deleted : " + customer.Name);
+        }
+
+        /*Customerları listeleme  metodu*/
+        public void List(params Customer[] customers)
+        {
+            Console.WriteLine("\nCustomers are listed");
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine("-----------------------------------");
+                Console.WriteLine("Customer name : " + customer.Name);
+                Console.WriteLine("Customer age : " + customer.Age);
+                Console.WriteLine("Customer gender : " + customer.Gender);
+                Console.WriteLine("Customer TcNo : " + customer.TcNo);
+            }
+        }
+    }
+}
---
+++ ClassMethodsDemos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ClassMethodsDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Yeni Customer nesneleri oluşturulup propertys girilir
+            Customer customer1 = new Customer();
+            customer1.Name = "David";
+            customer1.TcNo = "4003005555";
+            customer1.Age = 20;
+            customer1.Gender = "Male";
+            Customer customer2 = new Customer();
+            customer2.Name = "Josie";
+            customer2.TcNo = "5006005588";
+            customer2.Age = 35;
+            customer2.Gender = "Female";
+            Customer customer3 = new Customer();
+            customer3.Name = "Ivan";
+            customer3.TcNo = "7896321555";
+            customer3.Age = 41;
+            customer3.Gender = "Male";
+
+            //customer lar için bir dizi oluşturdum
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            //CustomerManager nesnesi oluşturuldu
+            CustomerManager customerManager = new CustomerManager();
+
+            //Add metotları çağırıldı
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+
+            //Listele metotu çağırıldı
+            customerManager.List(customers);
+
+        }
+    }
+}
